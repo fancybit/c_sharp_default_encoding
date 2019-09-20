35359df2f8431ae22478ae76719e2eaac08a9854
fancybit <fancybit@qq.com>
2019-09-20 19:24:40 +0800

    nothing to add

---
+++ EncodingTest/FileUTF8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodingTest
+{
+    public class FileUTF8
+    {
+        public static string getStr()
+        {
+            return "实践出真知";
+        }
+    }
+}
---
+++ EncodingTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodingTest
+{
+    class Program
+    {
+        unsafe public static void PrintBin(ref string s)
+        {
+            fixed (char* fp = s)
+            {
+                var p = (byte*)fp;
+                while(*p!=0)
+                {
+                    Console.Write((*p++).ToString("X"));
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintEncoding(string str, string encoding)
+        {
+            Console.WriteLine($"{encoding}:");
+            var bin = Encoding.GetEncoding(encoding).GetBytes(str);
+            for (var i = 0; i < bin.Length; ++i)
+            {
+                Console.Write(bin[i].ToString("X"));
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            var str = "实践出真知";
+            var str2312 = FileGB2312.getStr();
+            var str8 = FileUTF8.getStr();
+            var str16 = FileUTF16.getStr();
+            PrintBin(ref str);
+            PrintBin(ref str2312);
+            PrintBin(ref str8);
+            PrintBin(ref str16);
+
+            PrintEncoding(str,"utf-8");
+            PrintEncoding(str, "utf-16");
+            PrintEncoding(str, "gb2312");
+            PrintEncoding(str, "gbk");
+            PrintEncoding(str, "ASCII");
+            Console.ReadKey();
+        }
+    }
+}
